e1e75e940009e4c3e663fb73efbda4f3fd9422e5
Sijo Jose <sijo.jose@philips.com>
2019-08-14 11:41:54 +0530

    Update setup.py
    version updated

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
 
 setup(
     name='Zerto',
-    version='0.1dev',
+    version='0.2dev',
     description='Module for calling the Zerto api',
     author='Kees Bos',
     author_email='k.bos@capitar.com',
